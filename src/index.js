d73b886b60993b7582cdd73d046376dae0cb527f
Johan Leitet <johan.leitet@lnu.se>
2020-11-16 14:07:20 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+/**
+ * The main script file of the application.
+ *
+ * @author // TODO: YOUR NAME <YOUR EMAIL>
+ * @version 1.0.0
+ */
+
+const messageContainer = document.querySelector('#messageContainer')
+const message = document.createTextNode('Hi from an ECMAScript Module.')
+messageContainer.appendChild(message)
+
+// TODO: This is just some temple code that you are free to use, modify or completly delete. Add your code here instead.
